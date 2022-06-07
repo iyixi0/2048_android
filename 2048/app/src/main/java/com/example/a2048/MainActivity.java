f4fbd74a3ef2909cc2e2d13927b52ad8ea26fbf1
lyx <lvyuxing001@163.com>
2022-06-08 00:01:02 +0800

    base: new demoapp

--- 2048/app/src/main/java/com/example/a2048/MainActivity.java
+++ 2048/app/src/main/java/com/example/a2048/MainActivity.java
@@ -2,13 +2,35 @@ package com.example.a2048;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button bt_start,bt_quit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bt_start = findViewById(R.id.bt_start);
+        bt_quit = findViewById(R.id.bt_quit);
+
+        bt_start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,GameActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        bt_quit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.exit(0);
+            }
+        });
     }
 }
---
+++ 2048/app/src/main/java/com/example/a2048/gameMainView.java
@@ -0,0 +1,16 @@
+package com.example.a2048;
+
+import android.content.Context;
+import android.view.View;
+
+public class gameMainView extends View {
+
+    public gameMainView(Context context) {
+        super(context);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+}
